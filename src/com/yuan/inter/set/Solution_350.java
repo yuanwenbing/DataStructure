6a5cd5a203baa60f53ab3a8679401d38f6a154f3
yuanwb <yuanwb@yiche.com>
2019-10-28 17:41:10 +0800

    add solution

---
+++ src/com/yuan/inter/set/Solution_350.java
@@ -0,0 +1,34 @@
+package com.yuan.inter.set;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Solution_350 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums1) {
+            if (!map.containsKey(num))
+                map.put(num, 1);
+            else
+                map.put(num, map.get(num) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            }
+        }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
